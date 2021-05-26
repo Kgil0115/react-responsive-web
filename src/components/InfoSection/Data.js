2d9174fc2961e5e56dd8b2ee616f72811f317f03
kgil0115 <gilwon0115@gmail.com>
2021-05-26 23:00:52 +0900

    make three pages

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -8,10 +8,46 @@ export const homeObjOne = {
   description:
     "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
   buttonLabel: "Get started",
-  imgStart: false,
+  imgStart: true,
   img: require("../../images/img01.png"),
   alt: "Car",
   dark: true,
   primary: true,
+  darkText: true
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Unlimited Access",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/img01.png"),
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: require("../../images/img01.png"),
+  alt: "Paper",
+  dark: false,
+  primary: false,
   darkText: true
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,11 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
-import { homeObjOne } from "../components/InfoSection/Data";
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree
+} from "../components/InfoSection/Data";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,6 +21,8 @@ const Home = () => {
       <Navbar toggle={toggle} />
       <HeroSection></HeroSection>
       <InfoSection {...homeObjOne}></InfoSection>
+      <InfoSection {...homeObjTwo}></InfoSection>
+      <InfoSection {...homeObjThree}></InfoSection>
     </>
   );
 };
